b2e06c99706f6d56e7202efa6fcc942509c45f19
Devin Torres <devin@devintorr.es>
2013-04-22 13:20:00 -0500

    Faster check of datetime formats on decode, also 0.8.2 compatability

--- lib/json/decoder.ex
+++ lib/json/decoder.ex
@@ -20,7 +20,7 @@ defmodule JSON.Decoder do
       defexception DecodeError, message: "cannot decode"
 
       def decode(string, opts // []) do
-        opts = Keyword.merge([post_decode: function(:post_decoder, 1)], opts)
+        opts = Keyword.merge([post_decode: function(post_decoder/1)], opts)
         try do
           case :jsx.decode(string, opts) do
             { :incomplete, _ } -> raise alias!(DecodeError)
@@ -34,9 +34,8 @@ defmodule JSON.Decoder do
       def post_decoder(:null), do: nil
 
       def post_decoder(<<yyyy :: binary(4), ?-, mm :: binary(2), ?-, dd :: binary(2)>> = string) do
-        parts = [ yyyy, mm, dd ]
-        if all_digits?(parts) do
-          list_to_tuple(lc p inlist parts, do: binary_to_integer(p))
+        if string =~ %r/^\d{4}-\d{2}-\d{2}$/ do
+          list_to_tuple(lc p inlist [ yyyy, mm, dd ], do: binary_to_integer(p))
         else
           string
         end
@@ -44,10 +43,9 @@ defmodule JSON.Decoder do
 
       def post_decoder(<<yyyy :: binary(4), ?-, mm :: binary(2), ?-, dd :: binary(2), ?T,
                            hh :: binary(2), ?:, nn :: binary(2), ?:, ss :: binary(2), ?Z>> = string) do
-        parts = [ yyyy, mm, dd, hh, nn, ss ]
-        if all_digits?(parts) do
+        if string =~ %r/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}Z$/ do
           [ year, month, day, hour, minute, second ]
-            = lc p inlist parts, do: binary_to_integer(p)
+            = lc p inlist [ yyyy, mm, dd, hh, nn, ss ], do: binary_to_integer(p)
           { { year, month, day }, { hour, minute, second } }
         else
           string
@@ -55,10 +53,6 @@ defmodule JSON.Decoder do
       end
 
       def post_decoder(value), do: value
-
-      defp all_digits?(parts) do
-        Enum.all?(Enum.map parts, Regex.match? %r/^\d+$/, &1)
-      end
 
       defoverridable [decode: 1, decode: 2, post_decoder: 1]
     end
